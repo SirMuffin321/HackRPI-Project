7787bc6de42d1f430a7d817ea5d44eda91718557
Bubbable <yaminehacene2005@gmail.com>
2023-11-04 14:57:21 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,2 +1,13 @@
 print("Jello world")
 print('Munch')
+
+#Machine Learning with Health Care
+# - Diabetes
+# - Dementia
+# - Arthiritis
+# - Osteoporosis
+# - Stroke
+# - Dyslexia
+#   -One Idea could be determining whether someone has dyslexia or not using AI. This would work through pictures of brain scans. 
+
+
